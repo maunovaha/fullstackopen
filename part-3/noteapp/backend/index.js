440dd7e17eb629ff7db8d49688a6867590089652
Mauno Vähä <maunovaha@gmail.com>
2023-02-28 13:06:48 +0200

    Fixes noteapp to use mongodb for queries, and adds solutions for exercises 3.13 - 3.14

--- part-3/noteapp/backend/index.js
+++ part-3/noteapp/backend/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config(); // Reads the .env file variables
+
 const express = require('express');
 const cors = require('cors');
+const Note = require('./models/Note');
 const app = express();
 
 const requestLogger = (req, res, next) => {
@@ -15,67 +18,40 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
 
-const generateId = (collection) => {
-  const maxId = collection.length > 0 ? Math.max(...collection.map(n => n.id)) : 0;
-  return maxId + 1;
-}
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-];
-
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  Note.find({}).then(notes => {
+    res.json(notes);
+  });
 });
 
 app.post('/api/notes', (req, res) => {
   const { content, important } = req.body;
 
   if (!content) {
-    return res.status(400).json({
-      error: 'content missing'
-    });
+    return res.status(400).json({ error: 'content missing' });
   }
 
-  const note = { 
+  const note = new Note({
     content: content,
-    important: important || false,
-    id: generateId(notes)
-  };
+    important: important || false
+  });
 
-  notes = notes.concat(note);
-
-  res.json(note);
+  note.save().then(savedNote => {
+    res.json(savedNote);
+  });
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
+  Note.findById(req.params.id).then(note => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
+  /* TODO...
   const id = Number(req.params.id);
   notes = notes.filter(note => note.id !== id);
+  */
   res.status(204).end();
 });
 
@@ -85,7 +61,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
