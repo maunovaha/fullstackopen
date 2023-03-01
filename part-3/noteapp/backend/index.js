a48480e478b0300f8bffc1ba3702367ba0e69871
Mauno Vähä <maunovaha@gmail.com>
2023-03-01 13:13:03 +0200

    Develops the noteapp further and adds solutions for exercises 3.15 - 3.18

--- part-3/noteapp/backend/index.js
+++ part-3/noteapp/backend/index.js
@@ -13,53 +13,85 @@ const requestLogger = (req, res, next) => {
   next();
 }
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+}
+
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
 
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
+app.get('/api/notes', async (req, res, next) => {
+  try {
+    const notes = await Note.find({});
     res.json(notes);
-  });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res, next) => {
   const { content, important } = req.body;
 
   if (!content) {
     return res.status(400).json({ error: 'content missing' });
   }
 
-  const note = new Note({
-    content: content,
-    important: important || false
-  });
-
-  note.save().then(savedNote => {
+  try {
+    const note = new Note({ content: content, important: important || false });
+    const savedNote = await note.save();
     res.json(savedNote);
-  });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note);
-  });
+app.get('/api/notes/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  /* TODO...
-  const id = Number(req.params.id);
-  notes = notes.filter(note => note.id !== id);
-  */
-  res.status(204).end();
+app.put('/api/notes/:id', async (req, res, next) => {
+  const { content, important } = req.body;
+  const note = { content, important };
+
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, { new: true });
+    res.json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
 });
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
-}
+app.delete('/api/notes/:id', async (req, res, next) => {
+  try {
+    await Note.findByIdAndRemove(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
--- part-3/phonebook/backend/index.js
+++ part-3/phonebook/backend/index.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const Person = require('./models/Person');
 const app = express();
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+}
+
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
@@ -16,56 +24,86 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-app.get('/info', async (req, res) => {
-  const personCount = await Person.countDocuments({}).exec();
-  res.send(`
-    <p>Phonebook has info for ${personCount} people.</p>
-    <p>${new Date()}</p>
-  `);
+app.get('/info', async (req, res, next) => {
+  try {
+    const personCount = await Person.countDocuments({}).exec();
+    res.send(`
+      <p>Phonebook has info for ${personCount} people.</p>
+      <p>${new Date()}</p>
+    `);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/api/persons', async (req, res) => {
-  const people = await Person.find({});
-  res.json(people);
+app.get('/api/persons', async (req, res, next) => {
+  try {
+    const persons = await Person.find({});
+    res.json(persons);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.post('/api/persons', async (req, res) => {
+app.post('/api/persons', async (req, res, next) => {
   const { name, phoneNumber } = req.body;
 
   if (!name) {
     return res.status(400).json({ error: 'name is missing' });
   }
 
-  /* TODO...
-  if (persons.find(person => person.name === name)) {
-    return res.status(400).json({ error: 'name must be unique' });
-  }
-  */
-
   if (!phoneNumber) {
     return res.status(400).json({ error: 'phoneNumber is missing' });
   }
 
-  const person = new Person({ name, phoneNumber });
-  const savedPerson = await person.save();
+  try {
+    const existingPerson = await Person.findOne({ name: name }).exec();
 
-  res.json(savedPerson);
+    if (existingPerson) {
+      return res.status(400).json({ error: 'name must be unique' });
+    }
+
+    const person = new Person({ name, phoneNumber });
+    const savedPerson = await person.save();
+    res.json(savedPerson);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/api/persons/:id', async (req, res) => {
-  const person = await Person.findById(req.params.id);
-  res.json(person);
+app.get('/api/persons/:id', async (req, res, next) => {
+  try {
+    const person = await Person.findById(req.params.id);
+    res.json(person);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  /* TODO...
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-  */
-  res.status(204).end();
+app.put('/api/persons/:id', async (req, res, next) => {
+  const { name, phoneNumber } = req.body;
+  const person = { name, phoneNumber };
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(req.params.id, person, { new: true });
+    res.json(updatedPerson);
+  } catch (error) {
+    next(error);
+  }
 });
 
-const PORT = process.env.port;
+app.delete('/api/persons/:id', async (req, res, next) => {
+  try {
+    await Person.findByIdAndRemove(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
