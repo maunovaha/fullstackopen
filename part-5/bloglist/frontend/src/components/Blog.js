5188fe45be9d92a08616f15204affd5eaf23276c
Mauno Vähä <maunovaha@gmail.com>
2023-03-28 15:12:25 +0300

    Adds solution for exercise 5.7

--- part-5/bloglist/frontend/src/components/Blog.js
+++ part-5/bloglist/frontend/src/components/Blog.js
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+
 const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false);
+  const toggleLabel = viewDetails ? 'Hide' : 'View';
+
+  const toggleViewDetails = (e) => {
+    setViewDetails(!viewDetails);
+  };
+
   return (
-    <div>- {blog.title} (Written by <em>{blog.author || 'unknown author'}</em>)</div>
+    <>
+      <div style={{ display: 'flex', marginBottom: '0.25rem' }}>
+        <div style={{ paddingRight: '1rem' }}>
+          - {blog.title}
+        </div>
+        <div>
+          <button onClick={toggleViewDetails}>{toggleLabel}</button>
+        </div>
+      </div>
+      {viewDetails &&
+        <ul>
+          <li>Written by: {blog.author || 'unknown author'}</li>
+          <li>Url: {blog.url}</li>
+          <li>Likes: {blog.likes || 0} <button>Like</button></li>
+        </ul>
+      }
+    </>
   );
 };
 
