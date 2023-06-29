e06aefa85b67bea0f8419e8c4f68eacbbcef0216
Mauno Vähä <maunovaha@gmail.com>
2023-06-29 12:56:51 +0300

    Adds solution for exercise 8.9 - List all books (react app)

--- part-8/graphql-library-frontend/src/components/Books.js
+++ part-8/graphql-library-frontend/src/components/Books.js
@@ -1,13 +1,20 @@
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS);
+
   if (!props.show) {
     return null;
   }
 
-  const books = [];
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h2>books</h2>
+      <h2>Books</h2>
       <table>
         <tbody>
           <tr>
@@ -15,7 +22,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {result.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
