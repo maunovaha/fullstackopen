d4ea614b26869e5c2224463f13c904ef818ca583
Mauno Vähä <maunovaha@gmail.com>
2023-07-14 13:27:36 +0300

    Adds solution for exercise 9.5 - WebBMI

--- part-9/exercise-solutions/bmiCalculator.ts
+++ part-9/exercise-solutions/bmiCalculator.ts
@@ -18,7 +18,7 @@ const parseUserInput = (args: string[]): UserInput => {
   return { height, weight };
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = (weight / height / height) * 10000;
 
   if (bmi < 18.5) {
--- part-9/exercise-solutions/index.ts
+++ part-9/exercise-solutions/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.send({
+      error: 'malformatted parameters'
+    });
+  }
+
+  res.send({
+    height,
+    weight,
+    bmi: calculateBmi(height, weight)
+  });
 });
 
 const PORT = 3003;
