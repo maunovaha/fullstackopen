f96280260715dcc2d02048f4ff4eacfeaae11b15
Mauno Vähä <maunovaha@gmail.com>
2023-07-17 13:49:29 +0300

    Adds code related to part 9 (c) - Implementing the functionality

---
+++ part-9/flight-diary/src/services/diaryService.ts
@@ -0,0 +1,31 @@
+import diaryData from '../../data/entries.json';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+
+// Uses type assertion to fix compiler warning; Should be used only
+// when we know what we are doing!
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  // Even though we have declared `NonSensitiveDiaryEntry` we still need to explicitly remove the
+  // sensitive `comment` field from the returned array; The reason is that TS does not
+  // modify the actual data but only it's type (TS sucks in this regard, false sense of security?).
+  return diaries.map((item) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { comment, ...rest } = item;
+    return rest;
+  });
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries
+};
