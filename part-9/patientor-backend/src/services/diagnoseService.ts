d9388cc2f76a85c73a7ff57976c9dea8bea39840
Mauno Vähä <maunovaha@gmail.com>
2023-07-18 13:25:46 +0300

    Adds solution for exercise 9.10 - Patientor backend, step 3

---
+++ part-9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoseData from '../../data/diagnoses';
+import { DiagnoseEntry } from '../types';
+
+const getEntries = (): DiagnoseEntry[] => {
+  return diagnoseData;
+};
+
+export default {
+  getEntries
+};
