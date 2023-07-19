072bab1ad73d8a895b9f215587361842aa00aa9d
Mauno Vähä <maunovaha@gmail.com>
2023-07-19 13:17:39 +0300

    Adds code related to part 9 (d) - Deeper type usage and narrowing

--- part-9/react-ts-course-info/src/components/Content.tsx
+++ part-9/react-ts-course-info/src/components/Content.tsx
@@ -1,16 +1,34 @@
-interface Course {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
 interface ContentProps {
-  courses: Course[];
+  courses: CoursePart[];
 }
 
 const Content = (props: ContentProps) => {
   return (
     <>
-      {props.courses.map((course: Course) => (
+      {props.courses.map((course: CoursePart) => (
         <p key={course.name}>{course.name} {course.exerciseCount}</p>
       ))}
     </>
