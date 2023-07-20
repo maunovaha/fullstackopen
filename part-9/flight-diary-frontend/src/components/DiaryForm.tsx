0c074fa111a9210e591111b75cd1d884bb7d8fe6
Mauno Vähä <maunovaha@gmail.com>
2023-07-20 16:29:56 +0300

    Adds solution for exercise 9.19 - Changes in form element types

--- part-9/flight-diary-frontend/src/components/DiaryForm.tsx
+++ part-9/flight-diary-frontend/src/components/DiaryForm.tsx
@@ -1,6 +1,7 @@
 import { useState, SyntheticEvent } from 'react';
 import diaryService, { isFailedRequest } from '../services/diaryService';
 import Notification from './Notification';
+import { Visibility, Weather } from '../types';
 
 interface DiaryFormProps {
   onDiaryAdded: () => void;
@@ -8,8 +9,8 @@ interface DiaryFormProps {
 
 const DiaryForm = (props: DiaryFormProps) => {
   const [date, setDate] = useState('');
-  const [visibility, setVisibility] = useState('');
-  const [weather, setWeather] = useState('');
+  const [visibility, setVisibility] = useState<Visibility>(Visibility.Great);
+  const [weather, setWeather] = useState<Weather>(Weather.Sunny);
   const [comment, setComment] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -18,8 +19,8 @@ const DiaryForm = (props: DiaryFormProps) => {
     const data = await diaryService.addDiary({ date, visibility, weather, comment });
     if (!isFailedRequest(data)) {
       setDate('');
-      setVisibility('');
-      setWeather('');
+      setVisibility(Visibility.Great);
+      setWeather(Weather.Sunny);
       setComment('');
       setErrorMessage('');
       props.onDiaryAdded();
@@ -35,16 +36,40 @@ const DiaryForm = (props: DiaryFormProps) => {
       <form onSubmit={handleSubmit}>
         <div style={{ marginBottom: '1rem' }}>
           <label htmlFor="date" style={{ display: 'block' }}>Date:</label>
-          <input type="text" id="date" name="date" value={date} onChange={e => setDate(e.target.value)} />
+          <input type="date" id="date" name="date" value={date} onChange={e => setDate(e.target.value)} />
         </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="visibility" style={{ display: 'block' }}>Visibility:</label>
-          <input type="text" id="visibility" name="visibility" value={visibility} onChange={e => setVisibility(e.target.value)} />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="weather" style={{ display: 'block' }}>Weather:</label>
-          <input type="text" id="weather" name="weather" value={weather} onChange={e => setWeather(e.target.value)} />
-        </div>
+        <fieldset style={{ marginBottom: '1rem' }}>
+          <legend>Select the visibility:</legend>
+          {Object.values(Visibility).map((value: string) => (
+            <div key={value}>
+              <input
+                type="radio"
+                name="visibility"
+                id={value}
+                value={value}
+                onChange={e => setVisibility(e.target.value as Visibility)}
+                checked={visibility === value}
+              />
+              <label htmlFor={value}>{value}</label>
+            </div>
+          ))}
+        </fieldset>
+        <fieldset style={{ marginBottom: '1rem' }}>
+          <legend>Select the weather:</legend>
+          {Object.values(Weather).map((value: string) => (
+            <div key={value}>
+              <input
+                type="radio"
+                name="weather"
+                id={value}
+                value={value}
+                onChange={e => setWeather(e.target.value as Weather)}
+                checked={weather === value}
+              />
+              <label htmlFor={value}>{value}</label>
+            </div>
+          ))}
+        </fieldset>
         <div style={{ marginBottom: '1rem' }}>
           <label htmlFor="comment" style={{ display: 'block' }}>Comment:</label>
           <textarea id="comment" name="comment" value={comment} onChange={e => setComment(e.target.value)} />
